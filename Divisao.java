8f97bfad7aa27a1a329aef7107d3acbffbcce6a8
Paulo Vanderley <vanderley.1109@gmail.com>
2023-06-16 14:41:04 -0300

    adicionando conteudo

--- Divisao.java
+++ Divisao.java
@@ -1,3 +1,17 @@
-public class Divisao {
+package br.com.devsgeeknerd.calculadora.classes;
 
+public class Divisao extends CalculoMatematico {
+    public Divisao(int numero1, int numero2) {
+        super(numero1, numero2);
+    }
+
+    @Override
+    public Boolean validar() {
+        return numero1 >= 0 && numero2 > 0;
+    }
+
+    @Override
+    protected int doCalcular() {
+        return numero1 / numero2;
+    }
 }
